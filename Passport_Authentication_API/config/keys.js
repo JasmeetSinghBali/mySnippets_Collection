19192f9bec8061eb4ef24796d39843df3b46e6a8
Jasmeet Singh Bali <67728575+Jasmeet-1998@users.noreply.github.com>
2020-09-24 11:51:45 +0530

    Update keys.js

--- Passport_Authentication_API/config/keys.js
+++ Passport_Authentication_API/config/keys.js
@@ -1,6 +1,7 @@
 //store the mongoose url to connect to the database here
-
-dbPassword= 'mongodb+srv://devops1998:IidVbgLAdUPLMEZZ@cluster0.ajz47.mongodb.net/passport_authentication?retryWrites=true&w=majority';
+//Replace the <adminname> ,<passwordforadminuser> and <DBname> A/c To Your Credentials For Your MonogDbAtlas Account.
+//this URI Can Be Found When U Connect Your Application With MongoDBAtlas.
+dbPassword= 'mongodb+srv://<adminname>:<passwordforadminuser>@cluster0.ajz47.mongodb.net/<DBname>?retryWrites=true&w=majority';
 
 module.exports={
 	MongoURI: dbPassword
