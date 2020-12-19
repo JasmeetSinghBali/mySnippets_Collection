3d8553244b32450acc813c9a32ad909a22e61a44
JasmeetSinghBali <devs.us.1984@gmail.com>
2020-12-19 19:05:19 +0530

    Recaptcha V2 with Node

---
+++ recaptcha_node/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const { stringify } = require('querystring');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_, res) => res.sendFile(__dirname + '/index.html'));
+
+app.post('/subscribe', async (req, res) => {
+  if (!req.body.captcha)
+    return res.json({ success: false, msg: 'Please select captcha for Verification' });
+
+  // Secret key
+  const secretKey = '6LeKrgwaAAAAAL51xbiooGKjnqSM4vyRAwMqVgPR';
+
+  // Verify URL
+  const query = stringify({
+    secret: secretKey,
+    response: req.body.captcha,
+    remoteip: req.connection.remoteAddress
+  });
+  const verifyURL = `https://google.com/recaptcha/api/siteverify?${query}`;
+
+  // Make a request to verifyURL
+  const body = await fetch(verifyURL).then(res => res.json());
+
+  // If not successful
+  if (body.success !== undefined && !body.success)
+    return res.json({ success: false, msg: 'Failed captcha verification' });
+
+  // If successful
+  return res.json({ success: true, msg: 'Captcha passed' });
+});
+
+app.listen(3000, () => console.log('Server started on port 3000'));
